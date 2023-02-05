c1667e8787af4714649659f73f5a25434164dd28
Roman Perepechko <r.perepechko@idaproject.com>
2023-02-05 17:54:40 +0800

    init commit

---
+++ pages/catalog/product/_slug.vue
@@ -0,0 +1,17 @@
+<template>
+    <div :class="$style.ProductPage">
+        123
+    </div>
+</template>
+
+<script>
+export default {
+    
+};
+</script>
+
+<style lang="scss" module>
+    .ProductPage {
+
+    }
+</style>
